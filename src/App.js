79701d6b7186665a633ba66ae1a7b16929ae120f
TON RUGERIA <tonrugeria@gmail.com>
2023-04-27 01:58:11 +0800

    Component and Props
    Props lets you pass any data from a parent component to a child component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Component } from "react";
+import CardList from "./components/card-list/card-list.component";
+
 import "./App.css";
 
 class App extends Component {
@@ -9,22 +11,15 @@ class App extends Component {
       monsters: [],
       searchField: "",
     };
-    console.log("constructor");
   }
 
   componentDidMount() {
-    console.log("componentDidMount");
     fetch("http://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
-        this.setState(
-          () => {
-            return { monsters: users };
-          },
-          () => {
-            console.log(this.state);
-          }
-        )
+        this.setState(() => {
+          return { monsters: users };
+        })
       );
   }
 
@@ -37,8 +32,6 @@ class App extends Component {
   };
 
   render() {
-    console.log("render");
-
     const { monsters, searchField } = this.state;
     const { onSearchField } = this;
 
@@ -55,13 +48,7 @@ class App extends Component {
           onChange={onSearchField}
         />
 
-        {filteredMonster.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          );
-        })}
+        <CardList monsters={filteredMonster} />
       </div>
     );
   }
