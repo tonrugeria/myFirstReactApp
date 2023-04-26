ab6ca63bba125b7ff0c82295d1792a9154bbe757
TON RUGERIA <tonrugeria@gmail.com>
2023-04-27 01:12:17 +0800

    Optimization
    optimize onChange event and destructure this and this.state

--- src/App.js
+++ src/App.js
@@ -28,11 +28,22 @@ class App extends Component {
       );
   }
 
+  onSearchField = (event) => {
+    const searchField = event.target.value.toLowerCase();
+
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
     console.log("render");
 
-    const filteredMonster = this.state.monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(this.state.searchField);
+    const { monsters, searchField } = this.state;
+    const { onSearchField } = this;
+
+    const filteredMonster = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
     });
 
     return (
@@ -41,13 +52,7 @@ class App extends Component {
           className="search-box"
           type="search"
           placeholder="search monster"
-          onChange={(event) => {
-            const searchField = event.target.value.toLowerCase();
-
-            this.setState(() => {
-              return { searchField };
-            });
-          }}
+          onChange={onSearchField}
         />
 
         {filteredMonster.map((monster) => {
