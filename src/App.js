72e5b06f85bace1102c69a748a0fbc017af6f9f5
TON RUGERIA <tonrugeria@gmail.com>
2023-04-27 04:34:03 +0800

    Card Component

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1 className="app-title">Monsters Rolodex</h1>
         <SearchBox
           onChangeHandler={onSearchField}
           placeholder="search monsters"
